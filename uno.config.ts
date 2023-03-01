c2bff1812c52446e5382e54dd1ad50a8be678d19
maxdemaio <maxdman321@gmail.com>
2023-02-28 20:59:42 -0500

    rm typography and use custom css for prose

--- uno.config.ts
+++ uno.config.ts
@@ -3,17 +3,10 @@ import {
   presetIcons,
   presetUno,
   transformerDirectives,
-  presetTypography,
   presetWebFonts
 } from 'unocss'
 
 export default defineConfig({
-  shortcuts: [
-    { 'i-logo': 'i-logos-astro w-6em h-6em transform transition-800' },
-  ],
-  transformers: [
-    transformerDirectives(),
-  ],
   presets: [
     presetUno(),
     presetIcons({
@@ -28,51 +21,5 @@ export default defineConfig({
         mono: 'DM Mono',
       },
     }),
-    // custom 'prose' stylings
-    presetTypography({
-      cssExtend: {
-        p: {
-          "margin-bottom": "1rem"
-        },
-        hr: { 
-          "border-sizing": "border-box",
-          "border-top-width": "1px",
-          "border": "0 solid" 
-        },
-        pre: {
-          "padding": "0.75rem 1rem 0.75rem 1rem",
-					"font-family": "DM Mono",
-					"overflow-x": "auto",
-          "border-radius": "0.375rem",
-          "border-top-color": "black",
-          "border-style": "solid",
-          "white-space": "normal",
-          "line-height": "1.4",
-				},
-        "h1, h2, h3, h4, h5, h6": {
-					"font-family": "Kaisei Tokumin",
-          "font-weight": "700",
-          "margin": "1rem 0rem 1rem 0rem"
-				},
-        h1: {
-          "font-size": "2.25rem",
-          "line-height": "2.5rem",
-        },
-        "p > a, li > a": {
-					"color": "rgba(30,64,175)",
-          "text-decoration": "underline"
-				},
-        "p > a:hover, li > a:hover": {
-					"color": "rgba(30, 58, 138)",
-				},
-        "button > a": {
-          "color": "rgba(30,64,175)",
-          "text-decoration": "underline"
-        },
-        "button > a:hover": {
-          "color": "rgba(30, 58, 138)",
-        },
-      }
-    })
   ],
 })

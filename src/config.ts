63c22f9275a61b359cda1bdf305f02dfd39313ec
maxdemaio <maxdman321@gmail.com>
2023-03-03 13:06:15 -0500

    prettier apply

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const SITE_IMAGE = '/spirited-blog.jpg';
+export const SITE_TITLE = "My personal website.";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_IMAGE = "/spirited-blog.jpg";
--- uno.config.ts
+++ uno.config.ts
@@ -1,53 +1,54 @@
-import {
-  defineConfig,
-  presetIcons,
-  presetUno,
-  presetWebFonts
-} from 'unocss'
+import { defineConfig, presetIcons, presetUno, presetWebFonts } from "unocss";
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetIcons({
       extraProperties: {
-        'display': 'inline-block',
-        'vertical-align': 'text-bottom',
+        display: "inline-block",
+        "vertical-align": "text-bottom",
       },
     }),
     presetWebFonts({
       fonts: {
-        kai: 'Kaisei Tokumin',
-        mono: 'DM Mono',
+        kai: "Kaisei Tokumin",
+        mono: "DM Mono",
       },
     }),
   ],
   shortcuts: {
-    'mlink': 'text-primary-light dark:text-primary-dark border-b-1 border-primary-light dark:border-primary-dark',
-    'mproj': ''
+    mlink:
+      "text-primary-light dark:text-primary-dark border-b-1 border-primary-light dark:border-primary-dark",
+    mproj:
+      "transform transition duration-300 ease-in-out hover:bg-gray-100 dark:hover:bg-gray-900 rounded p-4",
   },
   theme: {
     colors: {
       // class="text-primary-light"
       // class="text-primary-dark"
-      'primary-light': '#224b37', 
-      'primary-dark': '#a7b7af'
+      "primary-light": "#224b37",
+      "primary-dark": "#a7b7af",
     },
     breakpoints: {
       // this overrides and doesn't merge
       // as per https://github.com/unocss/unocss/blob/main/README.md#extend-theme
-      md: '770px',
-      sm: '640px',
-      xs: '430px',
-    }
+      md: "770px",
+      sm: "640px",
+      xs: "430px",
+    },
   },
   rules: [
-    [/^text-(.*)$/, ([, c], { theme }) => {
-      if (theme.colors[c])
-        return { color: theme.colors[c] }
-    }],
-    [/^border-(.*)$/, ([, c], {theme}) => {
-      if (theme.colors[c])
-        return { 'border-color': theme.colors[c]}
-    }],
-  ]
-})
+    [
+      /^text-(.*)$/,
+      ([, c], { theme }) => {
+        if (theme.colors[c]) return { color: theme.colors[c] };
+      },
+    ],
+    [
+      /^border-(.*)$/,
+      ([, c], { theme }) => {
+        if (theme.colors[c]) return { "border-color": theme.colors[c] };
+      },
+    ],
+  ],
+});

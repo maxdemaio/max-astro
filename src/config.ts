6d96753551ca4f98c7485e6472be24513872958a
maxdemaio <maxdman321@gmail.com>
2023-03-04 00:04:54 -0500

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Max DeMaio';
+export const SITE_DESCRIPTION = 'Passionate about software, language, and business.';
 export const SITE_IMAGE = '/spirited-blog.jpg';

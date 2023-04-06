ba5d927b23221b46deb6d94f0f9fb3350ed25c6f
maxdemaio <maxdman321@gmail.com>
2023-04-06 15:56:32 -0400

    add tag enums and life boolean and styles for life

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,13 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
 
+// Define the allowed tag values using an enum
+export enum Tag {
+  SOFTWARE = 'software',
+  LANGUAGE = 'language',
+  BUSINESS = 'business',
+}
+
 // Define schemas for each collection we'd like to validate.
 // We could define more content collections below if we'd like
 const blog = defineCollection({
@@ -10,8 +17,9 @@ const blog = defineCollection({
     pubDate: z.string(),
     description: z.string(),
     draft: z.boolean().optional(),
-    tags: z.array(z.string()).optional(),
+    tags: z.array(z.nativeEnum(Tag)).optional(),
     image: z.string().optional(),
+    life: z.boolean().optional(),
   }),
 });
 

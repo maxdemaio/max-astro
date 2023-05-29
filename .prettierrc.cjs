429d7f3a14c4eee9c4bb2578aab544a929e85837
maxdemaio <maxdman321@gmail.com>
2023-05-28 20:38:59 -0400

    add cjs file for astro prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  tabWidth: 2,
+  printWidth: 200,
+  singleQuote: true,
+};

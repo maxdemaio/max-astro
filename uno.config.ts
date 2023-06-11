c47dfc6f99d9e1f00b85f2ba3241b9368bcad47c
maxdemaio <maxdman321@gmail.com>
2023-06-11 13:53:54 -0400

    code cleanup make text-lg on body

--- uno.config.ts
+++ uno.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
     presetIcons({
       extraProperties: {
         display: 'inline-block',
+        height: '1.2em',
+        width: '1.2em',
         'vertical-align': 'text-bottom',
       },
     }),

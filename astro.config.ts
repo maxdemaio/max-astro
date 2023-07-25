3713e19abc0edeb6387fcd8408c3b775cbfd029f
maxdemaio <maxdman321@gmail.com>
2023-07-25 17:46:27 -0400

    wip photos work for photo layout component

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 import sitemap from '@astrojs/sitemap';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maxdemaio.com',
-  integrations: [UnoCSS({ injectReset: true }), sitemap()],
+  integrations: [UnoCSS({
+    injectReset: true
+  }), sitemap(), mdx()],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark-dimmed',
-    },
-  },
+      theme: 'github-dark-dimmed'
+    }
+  }
 });

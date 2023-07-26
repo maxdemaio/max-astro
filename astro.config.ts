ec7588b3dfeb4b08cfe1e5fd4cd4378d9c05a5f0
maxdemaio <maxdman321@gmail.com>
2023-07-26 19:35:28 -0400

    add mdx and add web component for blogphotos component

--- astro.config.ts
+++ astro.config.ts
@@ -2,17 +2,21 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 import sitemap from '@astrojs/sitemap';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maxdemaio.com',
-  integrations: [UnoCSS({
-    injectReset: true
-  }), sitemap(), mdx()],
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    sitemap(),
+    mdx(),
+  ],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark-dimmed'
-    }
-  }
+      theme: 'github-dark-dimmed',
+    },
+  },
 });

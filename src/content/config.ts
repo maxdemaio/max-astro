c3f1cb82cc6ad67c37a6a9e7afb2d0b1015f0d77
maxdemaio <maxdman321@gmail.com>
2024-01-13 01:08:31 -0500

    biggie progress

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,7 @@ export enum Tag {
 // Define schemas for each collection we'd like to validate.
 // We could define more content collections below if we'd like
 const blog = defineCollection({
+  type: 'content',
   schema: z.object({
     fileName: z.string(),
     title: z.string(),
@@ -24,8 +25,25 @@ const blog = defineCollection({
   }),
 });
 
+// Define schemas for each collection we'd like to validate.
+// We could define more content collections below if we'd like
+const music = defineCollection({
+  type: 'content',
+  schema: z.object({
+    fileName: z.string(),
+    title: z.string(),
+    pubDate: z.string(),
+    description: z.string(),
+    duration: z.number(),
+    draft: z.boolean().optional(),
+    tags: z.array(z.nativeEnum(Tag)).optional(),
+    image: z.string().optional(),
+  }),
+});
+
 // Export a single `collections` object to register our collection(s)
 // This key should match your collection directory name in "src/content"
 export const collections = {
   blog: blog,
+  music: music,
 };
---
+++ src/content/music/test.md
@@ -0,0 +1,11 @@
+---
+fileName: test
+description: this is a test
+pubDate: 'Jan 13 2024'
+title: testing music post
+duration: 1
+---
+
+# hey this is a test music post
+
+## here is another test dog

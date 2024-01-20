256fc81ca245bcdefd1378d939f3c783bd412f9e
maxdemaio <maxdman321@gmail.com>
2024-01-20 18:37:24 -0500

    rm music

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,8 +7,6 @@ export enum Tag {
   SOFTWARE = 'software',
   LANGUAGE = 'language',
   BUSINESS = 'business',
-  LOGIC = 'logic',
-  PRODUCING = 'producing',
   LIFE = 'life',
 }
 
@@ -28,25 +26,8 @@ const blog = defineCollection({
   }),
 });
 
-// Define schemas for each collection we'd like to validate.
-// We could define more content collections below if we'd like
-const music = defineCollection({
-  type: 'content',
-  schema: z.object({
-    fileName: z.string(),
-    title: z.string(),
-    pubDate: z.string(),
-    description: z.string(),
-    duration: z.number(),
-    draft: z.boolean().optional(),
-    tags: z.array(z.nativeEnum(Tag)).optional(),
-    image: z.string().optional().default('/whisper-music.jpeg'), // default music image for og
-  }),
-});
-
 // Export a single `collections` object to register our collection(s)
 // This key should match your collection directory name in "src/content"
 export const collections = {
   blog: blog,
-  music: music,
 };

91be5a60676b52b89f912b27264572a6b1ae4daf
maxdemaio <maxdman321@gmail.com>
2024-01-29 12:31:02 -0500

    add hybrid output, static by default

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maxdemaio.com',
+  output: 'hybrid',
   integrations: [
     UnoCSS({
       injectReset: true,

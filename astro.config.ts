0b5dc7fbc22891f063c4ae219a4fc9efad3556c0
maxdemaio <maxdman321@gmail.com>
2024-02-14 12:59:11 -0500

    WIP WORK, YEP

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import type { RehypePlugins } from 'astro';
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,6 +22,39 @@ export default defineConfig({
     shikiConfig: {
       theme: 'github-dark-dimmed',
     },
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'wrap',
+          content: {
+            type: 'element',
+            tagName: 'span',
+            children: [
+              {
+                type: 'element',
+                tagName: 'svg',
+                properties: {
+                  width: 50,
+                  height: 50,
+                  fill: 'currentColor',
+                },
+                children: [
+                  {
+                    type: 'element',
+                    tagName: 'use',
+                    properties: {
+                      'xlink:href': '#deez',
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
+    ] as RehypePlugins,
   },
   adapter: vercel({
     webAnalytics: {
